64ab4e18420fb460cd0335900957c20dabb14101
mariano <magm3333@gmail.com>
2016-05-10 09:05:38 -0300

    fix yaml & CORS

--- seminarioDev2016/src/Web/src/main/java/com/magm/web/spring/config/WebConfig.java
+++ seminarioDev2016/src/Web/src/main/java/com/magm/web/spring/config/WebConfig.java
@@ -5,9 +5,18 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import com.magm.web.services.Constants;
 
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping(Constants.URL_BASE + "/**")
+			.allowedOrigins("*")
+			.allowedMethods("PUT", "DELETE", "GET",
+				"POST");
+	}
 
 	@Bean
 	public CommonsMultipartResolver multipartResolver() {
